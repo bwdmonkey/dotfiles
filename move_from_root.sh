edc81860bc6d00004746c3d186e8ccfd8b68d691
Tom Lee <win981026@gmail.com>
2019-06-01 23:23:41 -0700

    Add scripts to move dotfile from $HOME

---
+++ move_from_root.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ~/.bash_profile bash_profile
+cp ~/.bash_prompt bash_prompt
+cp ~/.bashrc bashrc
+cp ~/.gitconfig gitconfig
+cp ~/.vimrc vimrc
---
+++ move_to_root.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp bash_profile ~/.bash_profile
+cp bash_prompt ~/.bash_prompt
+cp bashrc ~/.bashrc
+cp gitconfig ~/.gitconfig
+cp vimrc ~/.vimrc
+
